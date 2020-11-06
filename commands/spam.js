88ab8c35f51c095274c54bc0cc915fb456f04306
Andy Wu <andywu.05@gmail.com>
2020-11-06 11:06:27 -0500

    Fixed DST issue

--- commands/spam.js
+++ commands/spam.js
@@ -3,7 +3,8 @@ module.exports = {
   description: "This is the spam command!",
   execute(message, args) {
     const dateFns = require("date-fns");
-    
+    const moment = require("moment");
+
     const messageConents = message.content;
 
     var numbCount = messageConents.substring(
@@ -21,13 +22,34 @@ module.exports = {
       messageConents.length
     );
 
-    const curTime = dateFns.format(new Date(dateFns.addHours(new Date(time), 4)), "yyyy-MM-dd'T'HH:mm");
+    let offset = 5;
+
+    const dtdate = moment(new Date());
+    var curDst = dtdate.isDST();
+    var prevDst = moment(dtdate).clone().subtract(1, "day").isDST();
+    var nextDst = moment(dtdate).clone().add(1, "day").isDST();
+    var isDstChangeDate =
+      (curDst !== nextDst) === true || (curDst === prevDst) !== true;
+
+    if (isDstChangeDate) {
+      offset = 4;
+    }
+
+    const curTime = dateFns.format(
+      new Date(dateFns.addHours(new Date(time), offset)),
+      "yyyy-MM-dd'T'HH:mm"
+    );
+    console.log(`Current time: ${curTime}`);
+
     const now = dateFns.format(new Date(), "yyyy-MM-dd'T'HH:mm");
+    console.log(`Now: ${now}`);
 
     var timeTillAction = dateFns.differenceInMilliseconds(
       new Date(curTime),
       new Date(now)
     );
+
+    console.log(`Time till action: ${timeTillAction}\n`);
 
     if (timeTillAction < 0) {
       timeTillAction = 0;
