6c787766a6c3b5a6b024918fd073c38ebbf00ab2
Andy Wu <andywu.05@gmail.com>
2020-11-13 12:35:21 -0500

    Added env support for discord bot key

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const Discord = require('discord.js');
-const {key} = require('./secret.js');
+const Discord = require("discord.js");
+let key = process.env.DISCORD_SPAM_KEY || "";
+
+if (key === "") {
+  const temp = require("./secret.js");
+  key = temp.key;
+}
 
 const client = new Discord.Client();
 
-const prefix = '!';
+const prefix = "!";
 
-const fs = require('fs');
+const fs = require("fs");
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+const commandFiles = fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command);
+  client.commands.set(command.name, command);
 }
 
-
-client.once('ready', () => {
-    console.log('Spam bot is online!');
+client.once("ready", () => {
+  console.log("Spam bot is online!");
 });
 
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    if(command === 'help') {
-        message.channel.send('!help - Display commands\n!spam  \n\t* Definition: !spam "Your text between quotes" #Number of times to repeat @Specify a certain date and time in 24hr format\n\t* Usage: !spam "Hello!" #5 @2020-10-19T09:54');
-    } else if (command === 'spam') {
-        client.commands.get('spam').execute(message, args);
-    }
+  if (command === "help") {
+    message.channel.send(
+      '!help - Display commands\n!spam  \n\t* Definition: !spam "Your text between quotes" #Number of times to repeat @Specify a certain date and time in 24hr format\n\t* Usage: !spam "Hello!" #5 @2020-10-19T09:54'
+    );
+  } else if (command === "spam") {
+    client.commands.get("spam").execute(message, args);
+  }
 });
 
 client.login(key);
